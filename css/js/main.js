f4213d60583035649b63ce96474a40647cc70d86
Abdullah <56775516+Zaazou@users.noreply.github.com>
2022-06-26 14:43:43 +0200

    header and first three sections

---
+++ css/js/main.js
@@ -0,0 +1,5 @@
+my_list = document.getElementById('nav-list');
+
+function showMenu() {
+    my_list.style.cssText = 'right:0';
+}
---
+++ js/main.js
@@ -0,0 +1,9 @@
+my_list = document.getElementById('nav-list');
+
+function showMenu() {
+    my_list.style.cssText = 'right:0';
+}
+
+function hideMenu() {
+    my_list.style.cssText = 'right:-200';
+}
